93520ea7cc3daa69b393a6e1f8e5de833b10f7f8
Tarek Ashraf <tarekshalaby2015@gmail.com>
2024-03-12 03:20:09 +0200

    game frontend

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask , render_template , redirect ,request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(port=5002,debug=True)
